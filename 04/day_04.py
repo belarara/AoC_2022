c00b79a9ec8f5ad302c885805156ad4134e6e1bc
bela <bela.britzke@gmx.net>
2022-12-04 06:36:10 +0100

    day 04 improve

--- 04/day_04.py
+++ 04/day_04.py
@@ -2,15 +2,5 @@ f = open("04/input", "r")
 data = [a for a in f.read().split("\n") if a != ""]
 pairs = [a.split(",") for a in data]
 pairs_split = [([int(a) for a in p.split("-")], [int(a) for a in q.split("-")]) for p,q in pairs]
-
-count = 0
-for ((a,b),(c,d)) in pairs_split:
-    if (a<=c and b>=d) or (c<=a and d>=b):
-        count += 1
-print(f"1) {count}")
-
-overlaps = 0
-for ((a,b),(c,d)) in pairs_split:
-    if a<=d and b>=c:
-        overlaps += 1
-print(f"2) {overlaps}")
+print(f"1) {sum([1 for ((a,b),(c,d)) in pairs_split if (a<=c and b>=d) or (c<=a and d>=b)])}")
+print(f"2) {sum([1 for ((a,b),(c,d)) in pairs_split if a<=d and b>=c])}")
