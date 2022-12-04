0d20be4eec286b3f6a4c859dd20bae9715528dc3
bela <bela.britzke@gmx.net>
2022-12-04 06:38:00 +0100

    day 04 improve3

--- 04/day_04.py
+++ 04/day_04.py
@@ -1,6 +1,4 @@
-f = open("04/input", "r")
-data = [a for a in f.read().split("\n") if a != ""]
-pairs = [a.split(",") for a in data]
-pairs_split = [([int(a) for a in p.split("-")], [int(a) for a in q.split("-")]) for p,q in pairs]
+with open("04/input", "r") as f:
+    pairs_split = [([int(a) for a in p.split("-")], [int(a) for a in q.split("-")]) for p,q in [a.split(",") for a in [a for a in f.read().split("\n") if a != ""]]]
 print(f"1) {sum([1 for ((a,b),(c,d)) in pairs_split if (a<=c and b>=d) or (c<=a and d>=b)])}")
 print(f"2) {sum([1 for ((a,b),(c,d)) in pairs_split if a<=d and b>=c])}")
