187c5dac3ab226a49303e82a9405f6452aa54096
bela <bela.britzke@gmx.net>
2022-12-25 10:25:42 +0100

    day 25 changed function names

--- 25/day_25.py
+++ 25/day_25.py
@@ -1,7 +1,7 @@
 with open("25/input", "r") as f:
     data = [a for a in f.read().split("\n") if a!=""]
 
-def str_to_int_list(s):
+def snafu_to_int_ls(s):
     return [{"=":-2, "-":-1, "0":0, "1":1, "2":2}[s[-i]] for i in range(1,len(s)+1)]
 
 def ls_to_int(ls, base=5):
@@ -26,5 +26,5 @@ def int_to_snafu_ls(num, base=5):
 def ls_to_str(ls):
     return "".join(["=-012"[ls[-i]+2] for i in range(1,len(ls)+1)])
 
-print(f"1) {ls_to_str(int_to_snafu_ls(sum([ls_to_int(str_to_int_list(o)) for o in data])))}")
+print(f"1) {ls_to_str(int_to_snafu_ls(sum([ls_to_int(snafu_to_int_ls(o)) for o in data])))}")
 print(f"2) ( •̀ ω •́ )✧")
